4df46279e8cca0515a17bd9947dff48b16b03923
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 17:35:35 +0200

    Added user actions

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { rootReducer } from "./root-reducer";
+
+const middlewares = [logger];
+
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
