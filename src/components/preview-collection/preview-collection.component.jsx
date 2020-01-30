520bb4b021709662310d7330c7310c1298e9ac00
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 18:54:39 +0200

    Adding items to cart

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -7,8 +7,8 @@ export const PreviewCollection = ({ title, items }) => (
   <div className="collection-preview">
     <h1>{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.slice(0, 4).map(({ id, ...itemProps }) => (
-        <CollectionItem key={id} {...itemProps} />
+      {items.slice(0, 4).map(item => (
+        <CollectionItem key={item.id} item={item} />
       ))}
     </div>
   </div>
