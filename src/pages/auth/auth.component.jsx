c9ad3461a1bed0e440192d6c7acd5fb1c0207388
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 15:01:47 +0200

    Added signup by email/password

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import "./auth.styles.scss";
 import { SignIn } from "../../components/sign-in/sign-in.component";
+import { SignUp } from "../../components/sign-up/sign-up.component";
 
 export const AuthPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
