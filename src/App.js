4efea24616bc588e162e2d172e0505518c2a833f
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 13:05:57 +0200

    Added auth page & sign in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
 import { Shop } from "./pages/shop/shop.component";
 import { Header } from "./components/header/header.component";
+import { AuthPage } from "./pages/auth/auth.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/signin" component={AuthPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./auth.styles.scss";
+import { SignIn } from "../../components/sign-in/sign-in.component";
+
+export const AuthPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
