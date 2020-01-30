90fadfe393439b41d5b6a37fa613704d667aa030
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 22:22:49 +0200

    Added collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./collection.styles.scss";
+
+export const CollectionPage = ({ match }) => (
+  <div className="collection">
+    <h2>COLLECTION PAGE</h2>
+  </div>
+);
