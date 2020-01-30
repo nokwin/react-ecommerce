cc768cd419a02f6087ab6c78a417bef34fade79f
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-01-30 21:28:56 +0200

    Added redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,22 @@
 import { combineReducers } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const rootReducerNotPersisted = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+export const rootReducer = persistReducer(
+  persistConfig,
+  rootReducerNotPersisted
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
 import logger from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+
 import { rootReducer } from "./root-reducer";
 
 const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const persistor = persistStore(store);
