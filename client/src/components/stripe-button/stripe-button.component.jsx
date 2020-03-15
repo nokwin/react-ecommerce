88e25ec8454856959733e5c3db61887f8edd1fc9
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-03-15 20:23:52 +0200

    Payment works

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = "pk_test_GX0PSq3H5qygz4B8pp6qIUSS";
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios
+      .post("/payment", {
+        amount: priceForStripe,
+        token
+      })
+      .then(resp => {
+        alert("Payment successful");
+      })
+      .catch(error => {
+        console.log("Payment error: ", error);
+        alert(
+          "There was an issue with your payment. Please make sure that you provided credit card"
+        );
+      });
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port, error => {
   if (error) throw error;
 
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
 
 app.post("/payment", (req, res) => {
