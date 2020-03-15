6b8fc4a1a3369db0bdcaca7d4d8c4e7999108ca3
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-03-15 20:43:13 +0200

    Shop data on useContext

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useContext } from "react";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
-
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionsContext from "../../context/collections/collections.context";
 
 import {
   CollectionPageContainer,
@@ -11,7 +9,11 @@ import {
   CollectionItemsContainer
 } from "./collection.styles";
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections.find(
+    c => c.routeName === match.params.collectionId
+  );
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
@@ -25,8 +27,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
