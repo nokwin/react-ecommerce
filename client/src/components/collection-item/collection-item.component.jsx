47f6189151c92458de4da522d92c8433e0f65b66
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-03-17 23:19:17 +0200

    Update cart dropdown on add item

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,4 @@
-import React, { useContext } from "react";
-
-import { CartContext } from "../../providers/cart/cart.provider";
+import React from "react";
 
 import {
   CollectionItemContainer,
@@ -11,9 +9,8 @@ import {
   PriceContainer
 } from "./collection-styles.styles";
 
-const CollectionItem = ({ item }) => {
+const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
-  const { addItem } = useContext(CartContext);
 
   return (
     <CollectionItemContainer>
