8af42489828c43db4caefb013ea93b85d5a1515e
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-03-17 22:23:30 +0200

    Added apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient, gql } from "apollo-boost";
 
 import "./index.css";
 import App from "./App";
 import CartProvider from "./providers/cart/cart.provider";
 import UserProvider from "./providers/user/user.provider";
 
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+client
+  .query({
+    query: gql`
+      {
+        getCollectionsByTitle(title: "hats") {
+          id
+          title
+          items {
+            id
+            name
+            price
+            imageUrl
+          }
+        }
+      }
+    `
+  })
+  .then(res => console.log(res));
+
 ReactDOM.render(
-  <UserProvider>
-    <CartProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </CartProvider>
-  </UserProvider>,
+  <ApolloProvider client={client}>
+    <UserProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </UserProvider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
